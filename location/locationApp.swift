648daf46e7f7e544d0e216e2db42a871c591b9db
liwei <liwei@vv.cn>
2024-12-04 15:13:00 +0800

    feat:  首次提交

---
+++ location/locationApp.swift
@@ -0,0 +1,17 @@
+//
+//  locationApp.swift
+//  location
+//
+//  Created by liwei on 2023/11/27.
+//
+
+import SwiftUI
+
+@main
+struct locationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
